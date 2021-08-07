b517791ddc2e5234810f0b6016e102462cf05c94
skendanavian <s.nissen.bass@gmail.com>
2021-08-06 23:34:19 -0600

    refactored tests

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// Part 4 - Handle all cases
+
+const add = (string) => {
+	if (!string) return 0;
+
+	// Defaults
+	let delimiter = ",";
+	let numString = string;
+	let negativeNumList = [];
+
+	// Handle custom delimiter cases
+	if (string.slice(0, 2) === "//") {
+		const splitDelimiter = string.split("\n");
+
+		//remove formatting before delimiter
+		delimiter = splitDelimiter[0].slice(2);
+
+		numString = splitDelimiter[1];
+	}
+
+	// Split string into array using delimiter
+	const numArray = numString.split(delimiter);
+
+	const sum = numArray.reduce(function addNumbers(sum, num) {
+		const number = parseInt(num, 10);
+		// if (typeof num !== "string") return sum + 0;
+		if (number < 0) negativeNumList.push(num);
+		return sum + parseInt(num, 10);
+	}, 0);
+
+	// Handle negative number edge case
+	if (negativeNumList.length) {
+		const negativeNumberString = negativeNumList.join(", ");
+		return `Negatives not allowed. The string contained ${negativeNumberString}`;
+	} else return sum;
+};
+
 // Part 1 - Add numbers in string using ',' as delimiter.
 
 const addString1 = (string) => {
@@ -53,42 +90,4 @@ const addString3 = (string) => {
 	return sum;
 };
 
-// Part 4 - Handle all cases
-
-const add = (string) => {
-	if (!string) return 0;
-
-	// Defaults
-	let delimiter = ",";
-	let numString = string;
-	let negativeNumList = [];
-
-	// Handle custom delimiter cases
-	if (string.slice(0, 2) === "//") {
-		const splitDelimiter = string.split("\n");
-
-		//remove formatting before delimiter
-		delimiter = splitDelimiter[0].slice(2);
-
-		numString = splitDelimiter[1];
-	}
-
-	// Split string into array using delimiter
-	const numArray = numString.split(delimiter);
-
-	const sum = numArray.reduce(function addNumbers(sum, num) {
-		const number = parseInt(num, 10);
-		console.log(number);
-		// if (typeof num !== "string") return sum + 0;
-		if (number < 0) negativeNumList.push(num);
-		return sum + parseInt(num, 10);
-	}, 0);
-
-	// Handle negative number edge case
-	if (negativeNumList.length) {
-		const negativeNumberString = negativeNumList.join(", ");
-		return `Negatives not allowed. The string contained ${negativeNumberString}`;
-	} else return sum;
-};
-
-module.exports = { addString1, addString2, addString3, add };
+module.exports = { add };
--- tests.js
+++ tests.js
@@ -1,73 +1,42 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const { add, addString1, addString2, addString3 } = require("./index");
+const { add } = require("./index");
 
 // Part 1
 const testCase1 = "1,2,5"; // expect 8
 const testCase2 = ""; // expect 0
 
-// describe("Part 1 - Comma Delimiter", function () {
-// 	it("should add the numbers in the string correctly and return an integer", () => {
-// 		expect(addString1(testCase1)).to.equal(8);
-// 	});
-// 	it("should return 0 when given an empty string as input", () => {
-// 		expect(addString1(testCase2)).to.equal(0);
-// 	});
-// });
-
 // Part 2
 const testCase3 = "1\n,2,3"; // expect 6
 const testCase4 = "1,\n2,4"; // expect 7
-
-// describe("Part 2 - Comma and newline delimiter", function () {
-// 	it("should add the numbers in the string correctly and return an integer", () => {
-// 		expect(addString2(testCase3)).to.equal(6);
-// 	});
-// 	it("should add the numbers in the string correctly and return an integer", () => {
-// 		expect(addString2(testCase4)).to.equal(7);
-// 	});
-// 	it("should return 0 when given an empty string as input", () => {
-// 		expect(addString2(testCase2)).to.equal(0);
-// 	});
-// });
 
 // Part 3
 const testCase5 = "//$\n1$2$3"; // expect 6
 const testCase6 = "//@\n2@3@8"; // expect 13
 const testCase7 = "//;\n1;3;4"; //expect 8
+
+// Part 4
 const testCase8 = "//;\n1;-5;-2"; // expect negative number exception
 const testCase9 = "//;\n1;9;-1"; // expect negative number exception
 
-// describe("Part 3 - Custom delimiter ", function () {
-// 	it("should add the numbers in the string correctly and return an integer", () => {
-// 		expect(addString3(testCase5)).to.equal(6);
-// 	});
-// 	it("should add the numbers in the string correctly and return an integer", () => {
-// 		expect(addString3(testCase6)).to.equal(13);
-// 	});
-// 	it("should add the numbers in the string correctly and return an integer", () => {
-// 		expect(addString3(testCase6)).to.equal(13);
-// 	});
-// 	it("should return 0 when given an empty string as input", () => {
-// 		expect(addString3(testCase2)).to.equal(0);
-// 	});
-// });
-
-describe("Combined All Cases", function () {
+describe("Part 1 - Comma Delimiter and Empty Strings return 0", function () {
 	it("should add the numbers in the string correctly and return an integer", () => {
 		expect(add(testCase1)).to.equal(8);
 	});
 	it("should return 0 when given an empty string as input", () => {
 		expect(add(testCase2)).to.equal(0);
 	});
+});
+describe("Part 2 - Comma and Newline Delimiter", function () {
 	it("should add the numbers in the string correctly and return an integer", () => {
 		expect(add(testCase3)).to.equal(6);
 	});
 	it("should add the numbers in the string correctly and return an integer", () => {
 		expect(add(testCase4)).to.equal(7);
 	});
-
+});
+describe("Part 3 - Custom Delimiter", function () {
 	it("should add the numbers in the string correctly and return an integer", () => {
 		expect(add(testCase5)).to.equal(6);
 	});
@@ -78,12 +47,14 @@ describe("Combined All Cases", function () {
 	it("should add the numbers in the string correctly and return an integer", () => {
 		expect(add(testCase7)).to.equal(8);
 	});
-	it("should throw an exception with the negative number(s) when there is a negative number in the group", () => {
+});
+describe("Part 4 - Negative Numbers throw and exception with the list of negatives", function () {
+	it("should throw the exception: 'Negatives not allowed. The string contained -5, -2'", () => {
 		expect(add(testCase8)).to.equal(
 			"Negatives not allowed. The string contained -5, -2"
 		);
 	});
-	it("should throw an exception with the negative number(s) when there is a negative number in the group", () => {
+	it("should throw the exception: 'Negatives not allowed. The string contained -1'", () => {
 		expect(add(testCase9)).to.equal(
 			"Negatives not allowed. The string contained -1"
 		);
