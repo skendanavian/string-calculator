952182f47a6fbb7f4fef2b034ed4f2fd6df3d6b2
skendanavian <s.nissen.bass@gmail.com>
2021-08-06 22:21:35 -0600

    part 3 passing

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-// Step 1
+// Part 1 - Add numbers in string using ',' as delimiter.
 
-const addString = (string) => {
+const addString1 = (string) => {
+	// Part 1 - Used string.split(',')
+	// Part 2 - Added regex to handle \n as .split() delimiter
+
 	if (!string) return 0;
 
 	const numArray = string.split(",");
 	const sum = numArray.reduce(function addNumbers(sum, num) {
+		if (typeof num !== "string") return sum + 0;
 		return sum + parseInt(num, 10);
 	}, 0);
+
+	console.log(sum);
 	return sum;
 };
 
-module.exports = addString;
+// Part 1 - Add numbers in string using ',' && \n as delimiter .
+
+const addString2 = (string) => {
+	// Part 1 - Used string.split(',')
+	// Part 2 - Added regex to handle \n as .split() delimiter
+
+	if (!string) return 0;
+
+	const numArray = string.split(/,|\n /);
+	const sum = numArray.reduce(function addNumbers(sum, num) {
+		if (typeof num !== "string") return sum + 0;
+		return sum + parseInt(num, 10);
+	}, 0);
+
+	console.log(sum);
+	return sum;
+};
+
+// Part 3 - Parse string and use a provided delimiter to split and sum the numbers in the string.
+
+const addString3 = (string) => {
+	if (!string) return 0;
+
+	const splitDelimiter = string.split("\n");
+
+	//remove formatting before delimiter
+	const delimiter = splitDelimiter[0].slice(2);
+
+	//split string into array based on provided delimiter
+	const numberArray = splitDelimiter[1].split(delimiter);
+
+	const sum = numberArray.reduce(function addNumbers(sum, num) {
+		if (typeof num !== "string") return sum + 0;
+		return sum + parseInt(num, 10);
+	}, 0);
+
+	return sum;
+};
+
+module.exports = { addString1, addString2, addString3 };
--- tests.js
+++ tests.js
@@ -1,16 +1,43 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const addString = require("./index");
+const { addString1, addString2, addString3 } = require("./index");
 
-const testCase1 = "1,2,5"; //expect 8
+// Part 1
+const testCase1 = "1,2,5"; // expect 8
 const testCase2 = ""; // expect 0
 
-describe("Add the numbers in the string", function () {
-	it("should add the numbers in the string", () => {
-		expect(addString(testCase1)).to.equal(8);
+describe("Part 1 - Comma Delimiter", function () {
+	it("should add the numbers in the string correctly and return an integer", () => {
+		expect(addString1(testCase1)).to.equal(8);
 	});
 	it("should return 0 when given an empty string as input", () => {
-		expect(addString(testCase2)).to.equal(0);
+		expect(addString1(testCase2)).to.equal(0);
+	});
+});
+
+// Part 2
+const testCase3 = "1\n,2,3"; // expect 6
+const testCase4 = "1,\n2,4"; // expect 7
+
+describe("Part 2 - Comma and newline delimiter", function () {
+	it("should add the numbers in the string correctly and return an integer", () => {
+		expect(addString2(testCase3)).to.equal(6);
+	});
+	it("should return 0 when given an empty string as input", () => {
+		expect(addString2(testCase4)).to.equal(7);
+	});
+});
+
+// Part 3
+const testCase5 = "//$\n1$2$3"; // expect 6
+const testCase6 = "//@\n2@3@8"; // expect 13
+
+describe("Part 3 - Custom delimiter ", function () {
+	it("should add the numbers in the string correctly and return an integer", () => {
+		expect(addString3(testCase5)).to.equal(6);
+	});
+	it("should return 0 when given an empty string as input", () => {
+		expect(addString3(testCase6)).to.equal(13);
 	});
 });
